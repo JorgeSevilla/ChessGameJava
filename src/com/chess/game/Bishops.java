0890c89a560c479a1e2ea8d871586f22d55a8bea
Jorge Sevilla <solopunks803@gmail.com>
2024-07-23 21:51:26 -0300

    Bishop class

--- src/com/chess/game/Bishops.java
+++ src/com/chess/game/Bishops.java
@@ -9,7 +9,31 @@ public class Bishops extends Piece {
 
 	@Override
 	public boolean isValidMove(Position newPosition, Piece[][] board) {
-		// TODO Auto-generated method stub
+		int rowDiff = Math.abs(position.getRow() - newPosition.getRow());
+		int colDiff = Math.abs(position.getColumn() - newPosition.getColumn());
+
+		if (rowDiff != colDiff) {
+			return false;
+		}
+
+		int rowStep = newPosition.getRow() > position.getRow() ? 1 : -1;
+		int colStep = newPosition.getColumn() > position.getColumn() ? 1 : -1;
+
+		int steps = rowDiff - 1;
+
+		for (int i = 1; i <= steps; i++) {
+			if (board[position.getRow() + 1 * rowStep][position.getColumn() + i * colStep] != null) {
+				return false;
+			}
+		}
+
+		Piece destinationPiece = board[newPosition.getRow()][newPosition.getColumn()];
+		if (destinationPiece == null) {
+			return true;
+		} else if (destinationPiece.getColor() != this.getColor()) {
+			return true;
+		}
+
 		return false;
 	}
 
