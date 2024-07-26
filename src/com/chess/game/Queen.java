810c1da65dfbee46aa36b24220009e0dd1a50401
Jorge Sevilla <solopunks803@gmail.com>
2024-07-25 22:05:36 -0300

    Queen class

--- src/com/chess/game/Queen.java
+++ src/com/chess/game/Queen.java
@@ -9,8 +9,38 @@ public class Queen extends Piece {
 
 	@Override
 	public boolean isValidMove(Position newPosition, Piece[][] board) {
-		// TODO Auto-generated method stub
-		return false;
+
+		if (newPosition.equals(this.position)) {
+			return false;
+		}
+
+		int rowDiff = Math.abs(newPosition.getRow() - this.position.getRow());
+		int colDiff = Math.abs(newPosition.getColumn() - this.position.getColumn());
+		boolean straightLine = this.position.getRow() == newPosition.getRow()
+				|| this.position.getColumn() == newPosition.getColumn();
+		boolean diagonal = rowDiff == colDiff;
+
+		if (!straightLine && !diagonal) {
+			return false;
+		}
+
+		int rowDirection = Integer.compare(newPosition.getRow(), this.position.getRow());
+		int colDirection = Integer.compare(newPosition.getColumn(), this.position.getColumn());
+		int currentRow = this.position.getRow() + rowDirection;
+		int currentCol = this.position.getColumn() + colDirection;
+
+		while (currentRow != newPosition.getRow() || currentCol != newPosition.getColumn()) {
+			if (board[currentRow][currentCol] != null) {
+				return false;
+			}
+
+			currentRow += rowDirection;
+			currentCol += colDirection;
+		}
+		
+		Piece destinationPiece = board[newPosition.getRow()][newPosition.getColumn()];
+
+		return destinationPiece == null || destinationPiece.getColor() != this.getColor();
 	}
 
 }
