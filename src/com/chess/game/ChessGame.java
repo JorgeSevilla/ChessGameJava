ecf93e4ed77263e70115a1774f9ad3e4cf5a40b8
Jorge Sevilla <solopunks803@gmail.com>
2024-07-30 20:38:57 -0300

    ChessGame class

---
+++ src/com/chess/game/ChessGame.java
@@ -0,0 +1,12 @@
+package com.chess.game;
+
+public class ChessGame {
+	
+	private ChessBoard board;
+	private boolean whiteTurn = true;
+	
+	public ChessGame () {
+		this.board = new ChessBoard();
+	}
+
+}
